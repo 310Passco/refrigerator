ed073d6ce24cab9ceeee723cf002b9f407150908
sato <burugari310@gmail.com>
2021-10-19 11:12:10 +0900

    master

--- coolboxapp/urls.py
+++ coolboxapp/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import FoodCreate, topfunc, list_view, count_minus, Food_Delete, login_view, logout_view, SignUpView
 urlpatterns = [
     path('create/', FoodCreate.as_view(), name='create'),
-    #path('top/', topfunc, name='top'),
     path('list/', list_view, name='list'),
     path('delete/<int:pk>', Food_Delete.as_view(), name='delete'),
     path('minus/<int:pk>', count_minus, name='minus'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup', SignUpView.as_view(), name='signup'),
+    #path('top/', topfunc, name='top'),
 
 ]
