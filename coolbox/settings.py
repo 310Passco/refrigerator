3142279c94d833272accdd89e0e908fe64bb071c
sato <burugari310@gmail.com>
2021-10-31 23:52:37 +0900

    main

--- coolbox/settings.py
+++ coolbox/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-czm9!*+1wc1s(sb0rio3-ephpi9-l4*g2=7sp0m2a_n4=yk^k9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["show-me-kigen.com","localhost","127.0.0.1"]
 
