da3d175c88f87979bdfbce8c3433b7b668f3207c
sato <burugari310@gmail.com>
2021-11-03 16:08:50 +0900

    main

--- coolbox/settings.py
+++ coolbox/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'coolbox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'refrigerator',
+      'USER': 'refrigeratoruser',
+      'PASSWORD': '310doragon',
+      'HOST': 'localhost',
+      'PORT': '',
     }
 }
 
