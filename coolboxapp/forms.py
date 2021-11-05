f24ecd942117ddd8eec95e8940604dbc130768f1
sato <burugari310@gmail.com>
2021-11-05 17:27:25 +0900

    main

--- coolboxapp/forms.py
+++ coolboxapp/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser, Food
-from django.forms import ModelForm
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
