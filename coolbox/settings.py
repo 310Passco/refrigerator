e5393e208c706d5958526f28d38ad108fbd580b2
sato <burugari310@gmail.com>
2021-11-05 21:02:27 +0900

    main

--- coolbox/settings.py
+++ coolbox/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-czm9!*+1wc1s(sb0rio3-ephpi9-l4*g2=7sp0m2a_n4=yk^k9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["show-me-kigen.com","localhost","127.0.0.1","160.251.55.172"]
 
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
-#if DEBUG:
-    #SECURE_SSL_REDIRECT = False
-#else:
-    #SECURE_SSL_REDIRECT = True
+if DEBUG:
+    SECURE_SSL_REDIRECT = False
+else:
+    SECURE_SSL_REDIRECT = True
